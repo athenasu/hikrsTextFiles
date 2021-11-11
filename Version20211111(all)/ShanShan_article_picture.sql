bf4ecd83b4392877a83dc5f6a7cc2d400aebf208
Athena <athenasu210@gmail.com>
2021-11-11 22:27:54 +0800

    更正character set: utf8mb4

--- Version20211111(all)/ShanShan_article_picture.sql
+++ Version20211111(all)/ShanShan_article_picture.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `ShanShan` /*!40100 DEFAULT CHARACTER SET big5 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `ShanShan`;
 -- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
 --
 -- Host: localhost    Database: ShanShan
@@ -52,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-11 21:51:29
+-- Dump completed on 2021-11-11 22:26:12
